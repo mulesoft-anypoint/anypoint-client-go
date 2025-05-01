fd5bfe538a54abfdea88afe9970cf78e5a5bcf32
pipeline-anypoint-clients-generator <pipeline@anypoint-client-generator.com>
2025-05-01 04:49:31 +0000

    Generates modules from pipeline.

--- org/api_default.go
+++ org/api_default.go
@@ -454,6 +454,113 @@ func (a *DefaultApiService) GetBGUsageExecute(r DefaultApiGetBGUsageRequest) (*U
 	return localVarReturnValue, localVarHTTPResponse, nil
 }
 
+type DefaultApiGetClientCredentialsRequest struct {
+	ctx context.Context
+	ApiService *DefaultApiService
+	orgId string
+	clientId string
+}
+
+func (r DefaultApiGetClientCredentialsRequest) Execute() (*Client, *http.Response, error) {
+	return r.ApiService.GetClientCredentialsExecute(r)
+}
+
+/*
+GetClientCredentials Method for GetClientCredentials
+
+Get client ID and client secret
+
+ @param ctx context.Context - for authentication, logging, cancellation, deadlines, tracing, etc. Passed from http.Request or context.Background().
+ @param orgId The ID of the organization in GUID format
+ @param clientId The client ID of the client
+ @return DefaultApiGetClientCredentialsRequest
+*/
+func (a *DefaultApiService) GetClientCredentials(ctx context.Context, orgId string, clientId string) DefaultApiGetClientCredentialsRequest {
+	return DefaultApiGetClientCredentialsRequest{
+		ApiService: a,
+		ctx: ctx,
+		orgId: orgId,
+		clientId: clientId,
+	}
+}
+
+// Execute executes the request
+//  @return Client
+func (a *DefaultApiService) GetClientCredentialsExecute(r DefaultApiGetClientCredentialsRequest) (*Client, *http.Response, error) {
+	var (
+		localVarHTTPMethod   = http.MethodGet
+		localVarPostBody     interface{}
+		formFiles            []formFile
+		localVarReturnValue  *Client
+	)
+
+	localBasePath, err := a.client.cfg.ServerURLWithContext(r.ctx, "DefaultApiService.GetClientCredentials")
+	if err != nil {
+		return localVarReturnValue, nil, &GenericOpenAPIError{error: err.Error()}
+	}
+
+	localVarPath := localBasePath + "/organizations/{orgId}/clients/{clientId}"
+	localVarPath = strings.Replace(localVarPath, "{"+"orgId"+"}", url.PathEscape(parameterValueToString(r.orgId, "orgId")), -1)
+	localVarPath = strings.Replace(localVarPath, "{"+"clientId"+"}", url.PathEscape(parameterValueToString(r.clientId, "clientId")), -1)
+
+	localVarHeaderParams := make(map[string]string)
+	localVarQueryParams := url.Values{}
+	localVarFormParams := url.Values{}
+
+	// to determine the Content-Type header
+	localVarHTTPContentTypes := []string{}
+
+	// set Content-Type header
+	localVarHTTPContentType := selectHeaderContentType(localVarHTTPContentTypes)
+	if localVarHTTPContentType != "" {
+		localVarHeaderParams["Content-Type"] = localVarHTTPContentType
+	}
+
+	// to determine the Accept header
+	localVarHTTPHeaderAccepts := []string{"application/json"}
+
+	// set Accept header
+	localVarHTTPHeaderAccept := selectHeaderAccept(localVarHTTPHeaderAccepts)
+	if localVarHTTPHeaderAccept != "" {
+		localVarHeaderParams["Accept"] = localVarHTTPHeaderAccept
+	}
+	req, err := a.client.prepareRequest(r.ctx, localVarPath, localVarHTTPMethod, localVarPostBody, localVarHeaderParams, localVarQueryParams, localVarFormParams, formFiles)
+	if err != nil {
+		return localVarReturnValue, nil, err
+	}
+
+	localVarHTTPResponse, err := a.client.callAPI(req)
+	if err != nil || localVarHTTPResponse == nil {
+		return localVarReturnValue, localVarHTTPResponse, err
+	}
+
+	localVarBody, err := io.ReadAll(localVarHTTPResponse.Body)
+	localVarHTTPResponse.Body.Close()
+	localVarHTTPResponse.Body = io.NopCloser(bytes.NewBuffer(localVarBody))
+	if err != nil {
+		return localVarReturnValue, localVarHTTPResponse, err
+	}
+
+	if localVarHTTPResponse.StatusCode >= 300 {
+		newErr := &GenericOpenAPIError{
+			body:  localVarBody,
+			error: localVarHTTPResponse.Status,
+		}
+		return localVarReturnValue, localVarHTTPResponse, newErr
+	}
+
+	err = a.client.decode(&localVarReturnValue, localVarBody, localVarHTTPResponse.Header.Get("Content-Type"))
+	if err != nil {
+		newErr := &GenericOpenAPIError{
+			body:  localVarBody,
+			error: err.Error(),
+		}
+		return localVarReturnValue, localVarHTTPResponse, newErr
+	}
+
+	return localVarReturnValue, localVarHTTPResponse, nil
+}
+
 type DefaultApiUpdateBGRequest struct {
 	ctx context.Context
 	ApiService *DefaultApiService
